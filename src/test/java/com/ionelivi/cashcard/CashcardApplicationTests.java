eab128b43ef81adf5c1cf86425d68f4fb7818d17
Liviu Ionesi <pehlivanu@gmail.com>
2024-05-28 16:02:54 +0200

    Initial commit

---
+++ src/test/java/com/ionelivi/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ionelivi.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
